d91fa078e5d0b3f4490d8a00b9405a6e50be9f85
kennn22 <kenedifp@gmail.com>
2023-06-08 22:40:23 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- <HelloWorld/> -->
+  <Quiz8/>
+
+  <div class="app">
+    <h1>Nama saya {{ name }}</h1>
+    <h2>{{ bilangan }}</h2>
+    <button v-on:click="tambah">Tambah</button>
+    <button v-on:click="kurang">Kurang</button>
+    <button v-on:click="kali">Kali</button>
+    <button v-on:click="bagi">bagi</button>
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Quiz8 from './components/Quiz8.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Quiz8,
+  },
+
+  data(){
+    return{
+      name : "Kenedi Fagus Pradana",
+      bilangan : 1,
+    }
+  },
+
+  methods: {
+    tambah(){
+      this.bilangan += 1;
+    },
+    kurang(){
+      this.bilangan -= 1;
+    },
+    kali(){
+      this.bilangan *= 5;
+    },
+    bagi(){
+      this.bilangan /= 5;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.app{
+  text-align: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+h2{
+  color: red;
+}
+
+button{
+  margin: 5px;
+  background-color: #B6E2A1;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/Quiz8.vue
@@ -0,0 +1,224 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src="../assets/logo.png"
+            alt="kenedi"
+            class="logo"
+            style="width: 110px"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <div class="container col-xxl-8 px-4 py-5">
+      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+          <lottie-player
+            src="https://assets3.lottiefiles.com/packages/lf20_sk5h1kfn.json"
+            background="transparent"
+            speed="1"
+            style="width: 100%; height: auto"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+        <div class="col-lg-6">
+          <h1 class="display-5 fw-bold lh-1 mb-3">
+            Jelajahi Dunia Baru: Meretas Jalur Menuju Future Life
+          </h1>
+          <p class="lead">
+            sebuah inisiatif yang mengajak orang-orang untuk menggali potensi dan peluang yang ada dalam era Future Life yang sedang berkembang. Dalam deskripsi ini, visi dari "Jelajahi Dunia Baru" adalah untuk mendorong eksplorasi, inovasi, dan perubahan positif dalam masyarakat.
+          </p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" class="btn btn-success btn-lg px-4 me-md-2">
+              Win
+            </button>
+            <button type="button" class="btn btn-danger btn-lg px-4">
+              Lose
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="produk text-center">
+    <div class="container">
+      <div class="align-item-center mb-3 row">
+        <div class="col-lg-9 ms-auto me-auto">
+          <h1 class="hi mb-1 text-dark">Future Life Dreams</h1>
+          <p class="fw-bold">
+            Undangan untuk berani melangkah, menggali potensi, dan membangun masa depan yang lebih baik bagi diri sendiri, masyarakat, dan generasi mendatang.
+          </p>
+
+          <div class="row">
+            <div class="col-lg-3 col-sm-6">
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="css-i6dzq1"
+              >
+                <path
+                  d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"
+                ></path>
+                <polygon points="12 15 17 21 7 21 12 15"></polygon>
+              </svg>
+              <h3 class="text-dark fw-bold">Seminar dan Konferensi</h3>
+              <p>
+                "Jelajahi Dunia Baru" dapat mengadakan seminar dan konferensi yang melibatkan pembicara terkemuka dan pakar di bidang teknologi, bisnis, sains, dan sosial. Acara ini bertujuan untuk menginspirasi dan memberikan wawasan terbaru tentang tren dan perubahan yang terjadi di masa depan.
+              </p>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="css-i6dzq1"
+              >
+                <path
+                  d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"
+                ></path>
+                <polygon points="12 15 17 21 7 21 12 15"></polygon>
+              </svg>
+              <h3 class="text-dark fw-bold">Program Pendidikan dan Pelatihan</h3>
+              <p>
+                Penyelenggaraan program pendidikan dan pelatihan yang dirancang untuk meningkatkan pengetahuan dan keterampilan individu dalam berbagai bidang yang relevan dengan masa depan. Program tersebut dapat mencakup pelatihan teknologi, keahlian digital, inovasi, kepemimpinan, dan pengembangan karier.
+              </p>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="css-i6dzq1"
+              >
+                <path
+                  d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"
+                ></path>
+                <polygon points="12 15 17 21 7 21 12 15"></polygon>
+              </svg>
+              <h3 class="text-dark fw-bold">Konsultasi dan Mentoring</h3>
+              <p>
+                penyediaan layanan konsultasi dan mentoring bagi individu yang ingin meretas jalur menuju Future Life. Para ahli dan mentor dapat memberikan panduan, saran, dan wawasan praktis dalam menghadapi tantangan dan mengembangkan potensi mereka.
+              </p>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="css-i6dzq1"
+              >
+                <path
+                  d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"
+                ></path>
+                <polygon points="12 15 17 21 7 21 12 15"></polygon>
+              </svg>
+              <h3 class="text-dark fw-bold">Komunitas Online</h3>
+              <p>
+                "Jelajahi Dunia Baru" dapat menyediakan platform online yang memungkinkan individu untuk terhubung dan berinteraksi dengan sesama peserta program. Komunitas ini dapat menjadi tempat berbagi pengetahuan, pengalaman, dan ide-ide, serta memfasilitasi kolaborasi dan pertukaran gagasan.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Quiz-8",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style>
+p{
+  padding-top: 10px;
+}
+
+h1{
+  font-size: 50px;
+  font-weight: bolder;
+}
+
+div ul li a:hover{
+  text-decoration: underline;
+}
+
+</style>
